87bcc76105badd12b444a34c656d8095935da4e4
juqn_l0v3 <Diegofigueroa0520@gmail.com>
2025-10-28 11:31:48 -0500

    firts commit

---
+++ Lunes.py
@@ -0,0 +1,8 @@
+nombre = input ("Escribe tu nombre: ")
+edad = int (input("Ingresa tu edad: "))
+
+print (f"Hola {nombre}, tienes {edad} años.")
+
+if edad >= 18:
+           print ("Eres Mayor de edad")
+else: print ("Eres Menor de edad")
