5ce2274397ea7ca7d04e5976ea747849f2a92ef3
stivenmoscoso <103283733+stivenmoscoso@users.noreply.github.com>
2025-11-19 13:21:44 -0500

    Delete Datos directory

--- Datos/archivo_csv.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-def crear_csv (nombre, encabezado):
-    with open(nombre, "w", newline="") as f:
-        write = csv.writer(f)   
-        write.writerow(encabezado)
--- Datos/main.py
+++
@@ -1,3 +0,0 @@
-from archivo_csv import crear_csv
-
-print(crear_csv ("Coders.csv", ["nombre", "Edad"]))
